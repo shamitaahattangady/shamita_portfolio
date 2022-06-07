3a4e8d30a12cb771ae5239bdaf1c07cdda98e607
shamitaahattangady <shamitahattangady@gmail.com>
2022-06-07 00:02:59 -0400

    Navbar iteration 3

--- src/components/Navbar/NavbarComponents.js
+++ src/components/Navbar/NavbarComponents.js
@@ -23,14 +23,3 @@ export default class NavbarComponents extends Component {
     }
 }
 
-/* <Navbar bg="light" variant="light">
-                    <Container>
-                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                            <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#features">Features</Nav.Link>
-                                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            </Nav>
-                    </Container>
-                </Navbar>
-*/
